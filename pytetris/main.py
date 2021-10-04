6c9ea88bb75ca40f2b39b8f44a8236fbce8bd0c7
Huy tran <huy.td183768@sis.hust.edu.vn>
2021-10-04 23:24:59 +0700

    draw simple cell

--- pytetris/main.py
+++ pytetris/main.py
@@ -1,6 +1,6 @@
 from enum import Enum
-from typing import List
-
+from dataclasses import dataclass
+from typing import Tuple
 import pygame, sys
 from pygame import draw
 from pygame import display
@@ -9,46 +9,68 @@ from pygame import event
 class Color(Enum):
     BLACK = 0, 0, 0
     WHITE = 255, 255, 255
+    RED = 255, 0, 0
 
+GridPos = Tuple[int, int]
 class Grid:
     def __init__(self, num_column: int, num_row: int, block_size: float) -> None:
         self._num_column = num_column
         self._num_row = num_row
         self.block_size = block_size
-        self.colors: List[List[Color]] = [[Color.WHITE for _ in range(num_column)] for _ in range(num_row)]
  
-    def draw(self):
+    def reset(self):
         for x in range(0, self._num_column):
             for y in range(0, self._num_row):
                 pos_x = x * self.block_size
                 pos_y = y * self.block_size
                 rect = pygame.Rect(pos_x, pos_y, self.block_size, self.block_size)
-                draw.rect(SCREEN, self.colors[y][x].value, rect)
+                draw.rect(SCREEN, Color.WHITE.value, rect, 1)
+    
+    def _validate_cell_pos(self, pos: GridPos):
+        return pos[0] < self._num_column and pos[1] < self._num_row
 
-    def update_color(self, color: Color, row: int, col: int):
-        self.colors[row][col] = color
+    def draw_cell(self, color: Color, pos: GridPos):
+        cell_game_pos_x = pos[0]*self.block_size
+        cell_game_pos_y = pos[1]*self.block_size
+        rect = pygame.Rect(cell_game_pos_x, cell_game_pos_y, self.block_size, self.block_size)
+        SCREEN.fill(Color.BLACK.value)
+        self.reset()
+        draw.rect(SCREEN, color.value, rect)
+        
 
+class Shape(Enum):
+    SQUARE = [[1, 1],
+              [1, 1]]
+
+class Brick:
+    def __init__(self, shape: Shape) -> None:
+        self.shape = shape
+    # def draw()
 
 def main():
     pygame.init()
     global SIZE, WIDTH, HEIGHT, SPEED, BLACK, WHITE, SCREEN
     SIZE = WIDTH, HEIGHT = 320, 480
     SPEED = [2, 2]
-    BLACK = 0, 0, 0
-    WHITE = 255, 255, 255
     SCREEN = display.set_mode(SIZE)
-    SCREEN.fill(BLACK)
+    SCREEN.fill(Color.BLACK.value)
+    grid = Grid(10, 24, 20)
+    grid.reset()
+    pos = (1, 1)
 
     while True:
-        grid = Grid(3, 5, 20)
-        grid.update_color(Color.BLACK, 0, 1)
-        grid.draw()
+        grid.draw_cell(Color.RED, pos)
         for e in event.get():
             if e.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit(0)
+            if e.type == pygame.KEYDOWN:
+                if e.key == pygame.K_DOWN:
+                    pos = (pos[0], pos[1] + 1)
+                if e.key == pygame.K_UP:
+                    pos = (pos[0], pos[1] - 1)
         
-        display.update()
+        display.flip()
 
 
 if __name__ == "__main__":
